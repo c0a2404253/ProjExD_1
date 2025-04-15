1b42c6abcd99a8e5229c05f834aab6403a9d697c
c0a2404253 <c0a2404253@edu.teu.ac.jp>
2025-04-15 16:40:25 +0900

    練習10 かぜでながされる

--- pygame_image.py
+++ pygame_image.py
@@ -29,6 +29,8 @@ def main():
             kk_rct.move_ip((-1,0))
         if key_list[pg.K_RIGHT]:
             kk_rct.move_ip((1,0))
+        else:
+            kk_rct.move_ip((-1,0))
         x = tmr % 3200#練習９
         screen.blit(bg_img, [-x, 0])#練習６
         screen.blit(bg2_img, [-x+1600, 0])#練習７
