e9b4e97fc88387382a774b6ead5fbc3cccdbf399
c0a2404253 <c0a2404253@edu.teu.ac.jp>
2025-04-15 16:50:16 +0900

    演習1-1　風に流される

--- pygame_image.py
+++ pygame_image.py
@@ -21,16 +21,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_list = pg.key.get_pressed()#ren10-3
+        kk_rct.move_ip((-1,0))
         if key_list[pg.K_UP]:
             kk_rct.move_ip((0,-1))
         if key_list[pg.K_DOWN]:
-            kk_rct.move_ip((0,1))
+            kk_rct.move_ip((0,+1))
         if key_list[pg.K_LEFT]:
             kk_rct.move_ip((-1,0))
         if key_list[pg.K_RIGHT]:
-            kk_rct.move_ip((1,0))
-        else:
-            kk_rct.move_ip((-1,0))
+            kk_rct.move_ip((+1,0))
+        # else:
+        #     kk_rct.move_ip((-1,0))
         x = tmr % 3200#練習９
         screen.blit(bg_img, [-x, 0])#練習６
         screen.blit(bg2_img, [-x+1600, 0])#練習７
