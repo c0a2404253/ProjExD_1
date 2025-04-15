4ecdc8948562607ac0e94ef54aadade7775f701a
c0a2404253 <c0a2404253@edu.teu.ac.jp>
2025-04-15 16:52:44 +0900

    演習1-2　右に進める

--- pygame_image.py
+++ pygame_image.py
@@ -21,7 +21,7 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         key_list = pg.key.get_pressed()#ren10-3
-        kk_rct.move_ip((-1,0))
+        # kk_rct.move_ip((-1,0))
         if key_list[pg.K_UP]:
             kk_rct.move_ip((0,-1))
         if key_list[pg.K_DOWN]:
@@ -30,8 +30,8 @@ def main():
             kk_rct.move_ip((-1,0))
         if key_list[pg.K_RIGHT]:
             kk_rct.move_ip((+1,0))
-        # else:
-        #     kk_rct.move_ip((-1,0))
+        else:
+            kk_rct.move_ip((-1,0))
         x = tmr % 3200#練習９
         screen.blit(bg_img, [-x, 0])#練習６
         screen.blit(bg2_img, [-x+1600, 0])#練習７
